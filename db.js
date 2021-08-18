bb5d08ef15dc8cc624dc5246d75ac044bb36de8b
ajithsundaram <48487056+ajithsundaram@users.noreply.github.com>
2021-08-19 00:01:20 +0530

    deleted

--- db.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin1:admin1@cluster0.1naqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
- console.log("db connected")
-  // perform actions on the collection object
-  client.close();
-});
